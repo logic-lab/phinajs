004d5ed120add9ebe1814c3272aeda9e69ba46a8
shigeyukiohsumi <s.ohsumi@logic-lab.net>
2018-12-10 18:38:49 +0900

    一口で食べろ：ねずみ・チーズ・テロップを整頓して追加。いらないコード・ファイルも削除

--- try-paku/main.js
+++ try-paku/main.js
@@ -6,16 +6,35 @@ var SCREEN_HEIGHT = 360;
 
 var ASSETS = {
   sound: {
-    title: './snd/opening.mp3',
-    game: './snd/game.mp3',
+    title: 'snd/opening.mp3',
+    game: 'snd/game.mp3',
+    success: 'snd/clear.mp3',
+    miss: 'snd/miss.mp3',
   },
   image: {
-    cheese: './img/cheese_w90h100.png',
-    rat: './img/mouse_w97h130.png',
-    title: './img/title.png',
-    background: './img/bg.png',
+    cheese: 'img/cheese_w90h100.png',
+    rat: 'img/mouse_w97h130.png',
+    telop: 'img/telop_w140h40.png',
+    title: 'img/title.png',
+    background: 'img/bg.png',
   },
   spritesheet: {
+    telop_ss: {
+      frame: {
+        width: 140,
+        height: 40,
+        cols: 2,
+        rows: 1,
+      },
+      animations: {
+        success: {
+          frames: [0],
+        },
+        miss: {
+          frames: [1],
+        },
+      },
+    },
     rat_ss: {
       frame: {
         width: 97,
@@ -26,15 +45,15 @@ var ASSETS = {
       animations: {
         stay: {
           frames: [0,1],
-          frequency: 15,
+          frequency: 13,
         },
         miss: {
           frames: [2,4],
-          frequency: 15,
+          frequency: 13,
         },
         success: {
           frames: [2,3],
-          frequency: 15,
+          frequency: 13 ,
         },
       },
     },
@@ -86,11 +105,33 @@ phina.define(
       anim.gotoAndPlay("stay");
       this.animation = anim;
     },
-    miss: function miss(){
+    miss: function(){
       this.animation.gotoAndPlay("miss");
     },
-    success: function miss(){
+    success: function(){
       this.animation.gotoAndPlay("success");
+    },
+  }
+);
+
+// テロップのスプライト
+phina.define(
+  'Telop',{
+    superClass: 'Sprite',
+    init: function(){
+      this.superInit('telop');
+      var anim = FrameAnimation('telop_ss').attachTo(this);
+      anim.gotoAndPlay("success");
+      this.animation = anim;
+      this.hide();
+    },
+    miss: function miss(){
+      this.animation.gotoAndPlay("miss");
+      this.show();
+    },
+    success: function(){
+      this.animation.gotoAndPlay("success");
+      this.show();
     },
   }
 );
@@ -100,10 +141,8 @@ phina.define(
   'TitleScene', {
     superClass: 'DisplayScene',
     init: function(option){
-      console.info(option);
       this.superInit(option);
       this.soundFinished = false;
-//      this.backgroundColor = '#000000';
       this.backgroundSprite = Sprite("title").addChildTo(this);
       this.backgroundSprite.x = this.gridX.center();
       this.backgroundSprite.y = this.gridY.center();
@@ -124,22 +163,19 @@ phina.define(
     
     superClass: 'DisplayScene',
     init: function(option) {
-      console.info(option);
       this.superInit(option);
       
       // ゲームのBGMを再生
-      SoundManager.play("game");
+      SoundManager.playMusic("game",0,false);
       
       // 背景色を指定
-//      this.backgroundColor = '#000000';
       this.backgroundSprite = Sprite("background").addChildTo(this);
       this.backgroundSprite.x = this.gridX.center();
       this.backgroundSprite.y = this.gridY.center();
       
       // ネズミの追加
-      // ToDo: ネズミの大きさがちがああああああう
       this.rat = Rat().addChildTo(this);
-      this.rat.x = this.gridX.center() - 5;
+      this.rat.x = this.gridX.center() - 8;
       this.rat.y = this.gridY.center() - 45;
       
       // チーズの追加
@@ -147,56 +183,44 @@ phina.define(
       this.cheese.x = this.gridX.center();
       this.cheese.y = this.gridY.center();
       
+      // テロップの追加
+      this.telop = Telop().addChildTo(this);
+      this.telop.x = this.gridX.center();
+      this.telop.y = 55;
+      
+      // クリック判定
+      this.playable = true;
+      
+      // 時間制限
       setTimeout(function(){
         this.showMiss();
       }.bind(this),5000);
       
     },
+    showSuccess: function(){
+      if( this.playable ){
+        this.playable = false;
+        SoundManager.stopMusic();
+        SoundManager.play("success");
+        this.rat.success();
+        this.telop.success();
+        this.cheese.hide();
+      }
+    },
     showMiss: function(){
-      // 失敗シーンを表示する
-//      this.exit("miss",{
-//        background: this.backgroundSprite,
-//        rat: this.rat,
-//        cheese: this.cheese,
-//      });
-//      this.exit();
+      if( this.playable ){
+        this.playable = false;
+        SoundManager.stopMusic();
+        SoundManager.play("miss");
+        this.rat.miss();
+        this.telop.miss();
+      }
     },
     onclick: function(){
-        this.showMiss();
-    },
-    update: function(app){
-    },
-  }
-);
-
-// 失敗のシーン
-phina.define(
-  'MissScene', {
-    
-    superClass: 'DisplayScene',
-    init: function(option) {
-      this.superInit(option);
-//      this.backgroundSprite = option.background.addChildTo(this);
-//      this.rat = option.rat.addChildTo(this);
-//      this.cheese = option.cheese.addChildTo(this);
-//      console.info( option );
-//      // 背景色を指定
-//      this.backgroundColor = '#000000';
-      this.backgroundSprite = Sprite("background").addChildTo(this);
-      this.backgroundSprite.x = this.gridX.center();
-      this.backgroundSprite.y = this.gridY.center();
-    },
-  }
-);
-
-// 成功のシーン
-phina.define(
-  'SuccessScene', {
-    
-    superClass: 'DisplayScene',
-    init: function(option) {
-      this.superInit(option);
-      console.info( option );
+      if( this.playable ){
+        this.showSuccess();
+        this.cheese.miss();
+      }
     },
   }
 );
@@ -206,7 +230,7 @@ phina.main(function() {
   
   // アプリケーション生成
   var app = GameApp({
-    startLabel: 'title', // メインシーンから開始する
+    startLabel: 'main',
     assets: ASSETS,
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
@@ -223,6 +247,8 @@ phina.main(function() {
       },
     ],
   });
+  
   // アプリケーション実行
   app.run();
+  
 });
